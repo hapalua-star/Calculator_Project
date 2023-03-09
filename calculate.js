cd6debc46547c3d15b097e136ea1d93dcbee8bc7
hapalua-star <69691701+hapalua-star@users.noreply.github.com>
2023-03-09 13:44:38 -0500

    readjust code in javascript file see if it makes numbers align correctly

--- calculate.js
+++ calculate.js
@@ -58,34 +58,60 @@ function operate(operator,a,b)
 
 
 	
+    
+const click1=document.querySelector(".button1");
+click1.addEventListener('click',printnumber1);
+
+
+
+ function printnumber1()
+   {
+       const number=document.createElement("p");
+       number.textContent= ("1");
+       const screen=document.getElementById("screen");
+       screen.appendChild(number);
        
+       
+   };
+
+  
+   const click2=document.querySelector(".button2");
+   click2.addEventListener('click',printnumber2);
+
+   function printnumber2()
+   {
+       const number=document.createElement("p");
+       number.textContent= ("2");
+       const screen=document.getElementById("screen");
+       screen.appendChild(number);
+       
+       
+       
+   };
 
 
-        
+   function switcher()
+   {
+    let screen=document.getElementById("screen");
+    let reverseScreen= screen.textContent.split("").reverse().join("");
+    
+    let reversedTextNode=document.createTextNode(reverseScreen);
+    screen.textContent="";
+    screen.appendChild(reversedTextNode);
 
-            
+   }
 
-const click1=document.querySelector(".button");
-click1.addEventListener('click',printnumber);
-
-
-
-           function printnumber()
-           {
-              const number=document.createElement("p");
-              number.textContent= ("1");
-              const screen=document.getElementById("screen");
-              screen.appendChild(number);
-           };
+   
+   
 
 
 
 
 
+    
 
 
 
-            
            
 });
 
